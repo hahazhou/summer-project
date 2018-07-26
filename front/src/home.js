7fdae43f7e7bd11f250a4a2ce58ff74421fb4ffc
DESKTOP-M6Q2UFK\asus <1048904006@qq.com>
2018-07-26 09:48:52 +0800

    样式调整

--- front/src/home.js
+++ front/src/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import userpic from './pictrue/userpic.jpg';
 import './App.css';
 import smap from './pictrue/smap.png';
 import {Link} from 'react-router-dom';
@@ -8,6 +7,7 @@ import qs from 'qs';
 import moment from 'moment';
 import {DatePicker} from 'antd';
 import 'antd/dist/antd.css';
+import userpic from './pictrue/userpic.jpg'
 
 class Home extends Component {
     constructor(props){
@@ -24,7 +24,7 @@ class Home extends Component {
             person:"person0",
             imgname:"znz.jpg",
             buff:"znz.jpg",
-            buthidden:true
+            buthidden:true,
         };
 
         this.camera=this.camera.bind(this);
@@ -83,14 +83,17 @@ class Home extends Component {
             person:this.state.person
         }))
             .then((response)=>{
+                alert(response.data.msg);
                 console.log(response);
             })
             .catch((error)=>{
                 console.log(error)
-            })
+            });
+        window.location.reload();
     };
     /***************************************************/
     sort=()=>{
+        this.setState({loading:false});
         axios.post("/controllerblock/sortVideoByTime",qs.stringify({
             camera:this.state.camera,
             time:this.state.time.format("YYYY-MM-DD").toString(),
@@ -102,7 +105,8 @@ class Home extends Component {
                     this.setState({
                         personlist:response.data.personlist,
                         buff:response.data.imgname,
-                        buthidden:false
+                        buthidden:false,
+                        loading:true
                     })
                 }
                 console.log(response);
@@ -151,48 +155,52 @@ class Home extends Component {
         return (
             <div className="Home">
                 <header className="App-header">
+                    <Link className={"homeLink"} to={"/admin"}>地图管理</Link><Link className={"homeLink"} to={"/login"}>返回登录</Link>
+                    <label className={"user"}>{this.state.user}</label>
+                    <img src={userpic} alt={"用户头像"} className={"userpic"}/>
                     <h1 className="App-title">首页-控制面板</h1>
-                    <img src={userpic} className="App-logo"  alt={"userpicture"}/><br/>
-                    <label>{this.state.user}</label><label>&nbsp;&nbsp;</label>
-                    <Link className={"sLink"} to={"/login"}>返回登录</Link>&nbsp;&nbsp;<Link className={"sLink"} to={"/admin"}>地图管理</Link>
                 </header>
-                <div className="videobox" >
-                    <h1>选定摄像头:<strong>{this.state.camera}</strong></h1>
-                    <button id={"now"} onClick={this.showVideo} className="blue">实时视频</button>
-                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                    <button onClick={this.showTime} id={"his"} className="blue">历史视频</button>
-                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                    <button className="blue" onClick={this.showimg} hidden={this.state.buthidden}>展示截图</button>
-                </div>
-                <div hidden={this.state.hidden}>
-                    <DatePicker defaultValue={this.state.time} className={"datapicker"} onChange={this.setDate}/>
-                    <label className={"hour"}>&nbsp;&nbsp;小时：</label>
-                    <select className={"hour"} onChange={this.setHour} >
-                    {
-                        this.state.hourlist.map(function (item) {
-                            return(<option>{item}</option>);
-                        })
-                    }
-                    </select>
-                    <label>&nbsp;&nbsp;&nbsp;</label>
-                    <button onClick={this.sort} className="blue">搜索</button>
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                    <div hidden={this.state.personhidden}>
-                        <img src={require("./pictrue/"+this.state.imgname)} alt={"截图无法显示"}/><br/>
-                        <select onChange={this.setPerson}>
-                            {this.state.personlist.map(function (person) {
+
+                <div className={"control"}>
+                    <div className="videobox" >
+                        <h1>选定摄像头:<strong>{this.state.camera}</strong></h1>
+                        <button id={"now"} onClick={this.showVideo} className="blue">实时视频</button>
+                        <button onClick={this.showTime} id={"his"} className="blue">历史视频</button>
+                        <button className="blue" onClick={this.showimg} hidden={this.state.buthidden}>展示截图</button>
+                    </div>
+
+                    <div hidden={this.state.hidden} className={"time"}>
+                        <DatePicker defaultValue={this.state.time} onChange={this.setDate}/>
+                        <label >&nbsp;&nbsp;小时：</label>
+                        <select onChange={this.setHour} >
+                        {
+                            this.state.hourlist.map(function (item) {
+                                return(<option>{item}</option>);
+                            })
+                        }
+                        </select>
+                        <button onClick={this.sort} className="blue">搜索</button>
+
+                        <div hidden={this.state.personhidden}>
+                            <img src={require("./pictrue/"+this.state.imgname)} alt={"截图无法显示"} className={"cut"}/><br/>
+                            <select onChange={this.setPerson}>
+                                {this.state.personlist.map(function (person) {
                                 return(<option id={person}>{person}</option>)
-                            })}
-                        </select><label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                        <button className="blue" onClick={this.selectPerson}>选定目标</button>
+                                })}
+                            </select>
+                            <button className="blue" onClick={this.selectPerson}>选定目标</button>
+                        </div>
+
                     </div>
                 </div>
-                <h1>建筑平面图</h1>
-                <p className={"ssmap"}>
-                <img className="smap" src={smap} border="0" useMap={'#mapmap'} alt={"map"}/>
-                </p>
+
+                <div className={"ssmap"}>
+                    <h1>建筑平面图</h1>
+                    <img className={"smap"} src={smap} border="0" useMap={'#mapmap'} alt={"map"}/>
+                </div>
+
                 <map id={'mapmap'} name={'mapmap'}>
-                    <area id="camera_0"  shape="rect" coords="270,90,360,160" onClick={this.camera} alt={"camera1"}/>
+                    <area id="camera_0"  shape="rect" coords="270,90,360,160" onClick={this.camera} alt={"camera1"} />
                     <area id="camera_1"  shape="rect" coords="630,205,700,275" onClick={this.camera} alt={"camera2"}/>
                     <area id="camera_2"  shape="rect" coords="270,300,340,370" onClick={this.camera} alt={"camera3"}/>
                 </map>
